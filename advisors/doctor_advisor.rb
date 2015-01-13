efc8a46ae606e61130e2f760c706d3512242bf1e
Dieter Pisarewski <dieter.pisarewski@gmail.com>
2015-01-13 19:29:29 +0100

    prettify queries

--- advisors/doctor_advisor.rb
+++ advisors/doctor_advisor.rb
@@ -1,16 +1,23 @@
 class DoctorAdvisor
   def get(symptoms, age, allergies = [], latitude = nil, longitude = nil)
-    query = Neo4j::Session.current.query
-    query = query.match('(patho:Pathology)-[:may_manifest_symptoms]->(symptoms:Symptom)').where("symptoms.name" => symptoms).with('patho')
-    query = query.match('(drug_class:DrugClass)-[cures:cures]->(patho)').where('cures.age_min <= {age} AND {age} < cures.age_max').params(age: age).with('drug_class')
-    query = query.match('(drug:Drug)-[:belongs_to_class]->(drug_class), (allergy:Allergy)')
-    query = query.where('NOT (drug)-[:may_cause_allergy]->(allergy) OR NOT(allergy.name IN {allergies})').params(allergies: allergies)
-    query = query.with('drug_class, drug')
-    query = query.match('(doctor:Doctor)-->(spe:DoctorSpecialization)-[:can_prescribe]->(drug_class)').
-      return('DISTINCT(doctor) AS doctor, 2 * 6371 * asin(sqrt(haversin(radians({lat} - COALESCE(doctor.latitude,{lat}))) + cos(radians({lat})) * cos(radians(COALESCE(doctor.latitude,90)))* haversin(radians({long} - COALESCE(doctor.longitude,{long}))))) AS distance').
-      params(lat: latitude, long: longitude).order('distance ASC')
-    query.inject({}) do |hash, result|
-      hash.merge!(result.doctor => result.distance)
-    end
+    Neo4j::Session.current.query.
+        match('(patho:Pathology)-[:may_manifest_symptoms]->(symptoms:Symptom)').
+        where('symptoms.name' => symptoms).
+        with('patho').
+        match('(drug_class:DrugClass)-[cures:cures]->(patho)').
+        where('cures.age_min <= {age} AND {age} < cures.age_max').
+        params(age: age).
+        with('drug_class').
+        match('(drug:Drug)-[:belongs_to_class]->(drug_class), (allergy:Allergy)').
+        where('NOT (drug)-[:may_cause_allergy]->(allergy) OR NOT(allergy.name IN {allergies})').
+        params(allergies: allergies).
+        with('drug_class, drug').
+        match('(doctor:Doctor)-->(spe:DoctorSpecialization)-[:can_prescribe]->(drug_class)').
+        return('DISTINCT(doctor) AS doctor, 2 * 6371 * asin(sqrt(haversin(radians({lat} - COALESCE(doctor.latitude,{lat}))) + cos(radians({lat})) * cos(radians(COALESCE(doctor.latitude,90)))* haversin(radians({long} - COALESCE(doctor.longitude,{long}))))) AS distance').
+        params(lat: latitude, long: longitude).
+        order('distance ASC').
+        inject({}) do |hash, result|
+          hash.merge!(result.doctor => result.distance)
+        end
   end
 end
--- advisors/drug_advisor.rb
+++ advisors/drug_advisor.rb
@@ -1,11 +1,17 @@
 class DrugAdvisor
   def get(symptoms, age, allergies = [])
-    query = Neo4j::Session.current.query
-    query = query.match('(patho:Pathology)-[:may_manifest_symptoms]->(symptoms:Symptom)').where("symptoms.name" => symptoms).with('patho')
-    query = query.match('(drug_class:DrugClass)-[cures:cures]->(patho)').where('cures.age_min <= {age} AND {age} < cures.age_max').params(age: age).with('drug_class')
-    query = query.match('(drug:Drug)-[:belongs_to_class]->(drug_class), (allergy:Allergy)')
-    query = query.where('NOT (drug)-[:may_cause_allergy]->(allergy) OR NOT(allergy.name IN {allergies})').params(allergies: allergies)
-    query = query.return('DISTINCT(drug) AS drug')
-    query.to_a.map(&:drug)
+    Neo4j::Session.current.query.
+        match('(patho:Pathology)-[:may_manifest_symptoms]->(symptoms:Symptom)').
+        where("symptoms.name" => symptoms).
+        with('patho').
+        match('(drug_class:DrugClass)-[cures:cures]->(patho)').
+        where('cures.age_min <= {age} AND {age} < cures.age_max').
+        params(age: age).
+        with('drug_class').
+        match('(drug:Drug)-[:belongs_to_class]->(drug_class), (allergy:Allergy)').
+        where('NOT (drug)-[:may_cause_allergy]->(allergy) OR NOT(allergy.name IN {allergies})').
+        params(allergies: allergies).
+        return('DISTINCT(drug) AS drug').
+        to_a.map(&:drug)
   end
 end
